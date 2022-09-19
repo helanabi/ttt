e03bd9b4e2d1e2a89f775b11df79c5a103e76280
Hassan El Anabi <helanabi@gmail.com>
2022-09-19 14:14:54 +0100

    initial commit

---
+++ lib.js
@@ -0,0 +1,79 @@
+const tasks = [
+    {
+	name: "fso",
+	duration: "36:07:00"
+    },
+    {
+	name: "cs61a",
+	duration: "987:12:15"
+    },
+    {
+	name: "mdn",
+	duration: "87:03:20"
+    },
+    {
+	name: "ttt",
+	duration: "00:30:24"
+    }
+];
+
+function list() {
+    if (tasks.length === 0)
+	console.log("No tasks are currenlty saved.\n" +
+		    "Use 'ttt -a TASK' to add one.");
+    else {
+	console.log("NAME\tDURATION");
+	tasks.map(task => `${task.name}\t${task.duration}`)
+	    .forEach(task => console.log(task));
+    }
+}
+
+function secondsSince(date) {
+    return Math.floor((new Date() - date) / 1000);
+}
+
+function makeTimeArray(seconds) {
+    return [
+	Math.floor(seconds / 3600),
+	Math.floor(seconds % 3600 / 60),
+	seconds % 60
+    ];	
+}
+
+function timeArrayToSeconds(timeArray) {
+    return timeArray[0]*3600 + timeArray[1]*60 + timeArray[2];
+}
+
+function addTime(timeArray, seconds) {
+    return makeTimeArray(timeArrayToSeconds(timeArray) + seconds);
+}
+
+function printTime(timeArray) {
+    if (process.stdout.isTTY) {
+	console.log("TODO"); 
+    } else {
+	console.log("Elapsed time:", timeArray.join(":"));
+    }
+}
+
+function start(taskName) {
+    const task = tasks.find(t => t.name === taskName);
+
+    if (!task) {
+	console.log(`Task '${taskName}' does not exist.\n` +
+		    `Use 'ttt -a ${taskName}' to add it.`);
+	process.exit();
+    }
+
+    const startTime = new Date();
+    const totalTime = task.duration.split(":").map(s => +s);
+
+    console.log(`Tracking task: ${taskName}`);
+
+    const print = () => printTime(addTime(totalTime, secondsSince(startTime)));
+
+    setInterval(print, 5000);
+    print();
+}
+
+module.exports = { list, start };
---
+++ ttt.js
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+
+const lib = require("./lib.js");
+
+const VERSION_NUM = "0.1.0";
+
+const doc = `
+   ttt - Task Time Tracker
+
+   Keep track of time spent on specific tasks
+
+
+   SYNOPSIS
+
+   ttt [TASK]
+   ttt -a TASK
+   
+
+   DESCRIPTION
+
+   Invokation with no arguments lists all known tasks.
+   If TASK is provided, it will cause the program to start tracking
+   time spent on the specified task.  Tracking can be paused then resumed
+   by pressing (CTRL+P), or halted by sending an Interrupt signal (CTRL+C).
+
+
+   OPTIONS
+
+   -a, --add TASK	Add a new task
+   -h, --help		Show help
+   -v, --version	Display version and copyright information
+
+
+   AUTHORS
+
+   Copyright (C) 2022 Hassan El anabi (al-annabi.tech)
+`;
+
+const version = `ttt version ${VERSION_NUM}
+Copyright (C) 2022 Hassan El anabi (al-annabi.tech)
+License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
+
+This is free software; you are free to change and redistribute it.
+There is NO WARRANTY, to the extent permitted by law.
+`;
+
+function main(argv) {
+    if (argv.length === 0)
+	lib.list();
+    else if (argv.length === 1 && argv[0] === "-h")
+	console.log(doc);
+    else if (argv.length === 1 && argv[0] === "-v")
+	console.log(version);
+    else if (argv.length === 1 && argv[0] !== "-a")
+	lib.start(argv[0]);
+    else if (argv.length === 2 && argv[0] === "-a")
+	lib.add(argv[1]);
+    else {
+	console.error("Invalid usage.  Use -h for help");
+	process.exit(1);
+    }
+}
+
+main(process.argv.slice(2));
